c763d50434404d06676f55743ffdc342b8460902
Itsjeet007y1 <jeet.62.jp@gmail.com>
2024-01-31 12:34:30 +0530

    added second commit

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -16,6 +16,7 @@ public class Test {
 		System.out.println("Git merge testing");
 		System.out.println("Created practice1 branch.");
 		System.out.println("Added first commit.");
+		System.out.println("Added second commit.");
 		for(int i = 0; i < 10; i ++) {
 			System.out.println(LocalDateTime.now().getNano());
 			System.out.println("Parent: "+ i);
